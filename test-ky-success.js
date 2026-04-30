c47e20525e574607617fc7db37207cf5e7382225
Jeremy Coe <jeremy@coe.sh>
2026-04-30 17:38:45 -0400

    Fix test logging: console.error instead of process.stderr.write
    Switched from process.stderr.write (buffered, invisible in
    background mode) to console.error (line-buffered, captured).
    Removed debug temp files.

--- test-ky-success.js
+++ test-ky-success.js
@@ -20,7 +20,7 @@ function log(msg) {
     year: 'numeric', month: '2-digit', day: '2-digit',
     hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false,
   }).replace(',', '');
-  process.stderr.write(`${local} EDT | ${msg}\n`);
+  console.error(`${local} EDT | ${msg}`);
 }
 
 async function run() {
